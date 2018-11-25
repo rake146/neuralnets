fa4b7c756ee2e8f6492934aab8ed01c59bc5e349
rake146 <jake.pinchbeck@hotmail.co.uk>
2018-11-25 14:50:07 +0000

    Scale + multiplty & add functions

--- NN/Matrix.js
+++ NN/Matrix.js
@@ -13,19 +13,46 @@ class Matrix
         }
       }
     }
-    scale(){
-      for (var i = 0; i < this.rows; i++)
-        for (var j = 0; j < this.columns; j++)
-          this.matrix[i][j] *= n;
+    // adding a scalar or matrix to base matrix
+    multiply(n){
+      if (n instanceof Matrix){
+        var matrixTemp = new Matrix(this.colums, n.columns);
+        var tempNum = 0;
+        // n will be second parameter of matrix calculation
+        for (var i = 0; i < this.rows; i++){
+          for (var j = 0; i < this.columns; j++){
+
+          }
+        }
+      }
+      else
+        for (var i = 0; i < this.rows; i++)
+          for (var j = 0; j < this.columns; j++)
+            this.matrix[i][j] *= n;
     }
-    add(){
-      for (var i = 0; i < this.rows; i++)
-        for (var j = 0; j < this.columns; j++)
-          this.matrix[i][j] += n;
+    // adding a scalar or matrix to base matrix
+    add(n){
+      if (n instanceof Matrix)
+        for (var i = 0; i < this.rows; i++)
+          for (var j = 0; j < this.columns; j++)
+            this.matrix[i][j] += n.matrix[i][j];
+      else
+        for (var i = 0; i < this.rows; i++)
+          for (var j = 0; j < this.columns; j++)
+            this.matrix[i][j] += n;
+    }
+    // transpose a matrix
+    transpose(){
+
     }
 }
 
 var mat = new Matrix(3,2);
+var mat2 = new Matrix(3,2);
 mat.add(2);
 mat.scale(3);
+mat2.add(1);
+mat.add(mat2);
+
+console.table(mat2.matrix);
 console.table(mat.matrix);
